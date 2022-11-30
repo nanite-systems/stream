372fb348b9942baf13cd0d42850485fcaa37dbf5
Microwavekonijn <microwavekonijn@pm.me>
2022-11-30 13:44:09 +0100

    feat(environment): added wildcard environment

--- apps/stream-manifold/src/environments/environment.manifest.ts
+++ apps/stream-manifold/src/environments/environment.manifest.ts
@@ -7,6 +7,16 @@ export class EnvironmentManifest {
     ps2: new EnvironmentDescription(['1', '10', '13', '17', '19', '40']),
     ps2ps4eu: new EnvironmentDescription(['2000']),
     ps2ps4us: new EnvironmentDescription(['1000']),
+    all: new EnvironmentDescription([
+      '1',
+      '10',
+      '13',
+      '17',
+      '19',
+      '40',
+      '1000',
+      '2000',
+    ]),
   };
 
   static validateEnvironmentKey(key: string): boolean {
