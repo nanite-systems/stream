fd8ca79d862102342f84b67a0ebe9bb358a7df8d
Microwavekonijn <microwavekonijn@pm.me>
2023-03-07 22:05:02 +0100

    fix: dependencies

--- shared/service-states/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './service-state.type';
-export * from './service-state.tracker';
--- shared/service-states/src/service-state.type.ts
+++
@@ -1,6 +0,0 @@
-export interface ServiceState {
-  worldId: string;
-  worldName: string;
-  detail: string;
-  online: boolean;
-}
